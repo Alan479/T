13bdb16fdea7eb9d37d3b17d110e7b2051c8249a
byniqing <byniqing@163.com>
2014-03-31 15:37:53 +0800

    初

---
+++ TesOne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TesOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // not dd
+            }
+        }
+    }
+}
